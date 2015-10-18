e8612bc1da677d4ebb4904fbf7bb796a8bb3d5c3
rockmanck <rockman.roll@gmail.com>
2015-10-19 00:54:22 +0300

    configuring database

--- src/main/java/ua/lifebook/config/AppConfig.java
+++ src/main/java/ua/lifebook/config/AppConfig.java
@@ -1,10 +1,12 @@
 package ua.lifebook.config;
 
 import com.typesafe.config.Config;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import ua.lifebook.jdbc.LifeBookJdbc;
 import ua.lifebook.notification.MailManager;
 
 @Configuration
@@ -12,9 +14,17 @@ import ua.lifebook.notification.MailManager;
 public class AppConfig {
 	private final Config config = new ConfigurationHolder().getConfig();
 
-    @Bean
-    MailManager mailManager() {
+    @Bean public MailManager mailManager() {
         return new MailManager(sender(), config.getString("lb.transport.mailSender.from"));
+    }
+
+    @Bean public LifeBookJdbc lifeBookJdbc() {
+        final LifeBookJdbc jdbc = new LifeBookJdbc();
+        final Config db = this.config.getConfig("lb.db");
+        final BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUsername(db.getString("username"));
+        jdbc.setDataSource(dataSource);
+        return jdbc;
     }
 
     private JavaMailSenderImpl sender() {
---
+++ src/main/java/ua/lifebook/jdbc/LifeBookJdbc.java
@@ -0,0 +1,6 @@
+package ua.lifebook.jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class LifeBookJdbc extends JdbcTemplate {
+}

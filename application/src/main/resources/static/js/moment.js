d8e398bea08fc801f1110814592a273ba7d9903d
rockmanck <rockman.roll@gmail.com>
2022-01-07 10:34:05 +0200

    Reverted moment.js removal

---
+++ application/src/main/resources/static/js/moment.js
@@ -0,0 +1,44 @@
+function MomentClass() {
+    this.edit = function(id, viewType) {
+        loadMomentById(id,  new Date(), viewType);
+    };
+
+    this.new = function(datePickerId, viewType) {
+        var dateVal = $('#' + datePickerId).data('date');
+        loadMomentById(-1, new Date(dateVal), viewType);
+    };
+
+    this.save = function() {
+        var momentModal = $('#momentModal');
+        var form = momentModal.find('form');
+        var viewType = form.find('#viewType').val();
+        var data = form.serialize();
+        $.post(form.attr('action'), data).done(function () {
+            momentModal.modal('hide');
+            switch (viewType) {
+                case 'ViewType.DAILY': Plan.loadDailyPlans(); break;
+                case 'ViewType.WEEKLY': Plan.loadWeeklyPlans(); break;
+            }
+        });
+    };
+
+    function loadMomentById(id, defaultDate, viewType) {
+        $.get('./moment/' + id + '/edit.html', function (data) {
+            var form = $('#momentModal');
+            form.html(data);
+            updateDate(form, defaultDate);
+            form.modal({backdrop: 'static'});
+            form.find('#viewType').val(viewType);
+        });
+    }
+
+    function updateDate(form, defaultDate) {
+        var dueDate = form.find('#momentDateRaw').val();
+        var date = dueDate !== '' ? new Date(dueDate) : defaultDate;
+        form.find('#momentDate').datetimepicker({
+            format: 'MM/DD/YYYY'
+        }).data("DateTimePicker").date(date);
+    }
+}
+
+var Moment = new MomentClass();

dda4a95d78a3f2b3390f4308b07f615071309de5
rockmanck <rockman.roll@gmail.com>
2023-05-29 17:16:26 +0300

    Fixed NPE for plan save without tags

--- infrastructure/rest-api/src/main/java/pp/ua/lifebook/web/tag/TagDtoMapper.java
+++ infrastructure/rest-api/src/main/java/pp/ua/lifebook/web/tag/TagDtoMapper.java
@@ -5,6 +5,11 @@ import pp.ua.lifebook.tag.Tag;
 import java.util.List;
 
 public class TagDtoMapper {
+
+    private TagDtoMapper() {
+        throw new UnsupportedOperationException("It is not allowed to create instance of this class");
+    }
+
     public static TagDto toDto(Tag tag) {
         TagDto dto = new TagDto();
         dto.setValue(tag.id());
@@ -14,6 +19,8 @@ public class TagDtoMapper {
     }
 
     public static List<Tag> toDomain(List<TagDto> dtos, int userId) {
+        if (dtos == null) return List.of();
+
         return dtos.stream()
             .map(dto -> new Tag(
                 dto.getValue(),
